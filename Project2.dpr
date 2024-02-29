b8d8868114e33c2c1e19c76f30c68c678604b9b1
JaheonKu <jaheon@wayties.com>
2024-02-29 15:04:20 +0900

    최초 commit

---
+++ Project2.dpr
@@ -0,0 +1,14 @@
+program Project2;
+
+uses
+  Vcl.Forms,
+  Unit2 in 'Unit2.pas' {Form17};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm17, Form17);
+  Application.Run;
+end.
